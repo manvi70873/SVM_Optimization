82838eb20211550311bee117c9bd9d39d1d7cbec
manvi70873 <72308786+manvi70873@users.noreply.github.com>
2023-04-20 01:22:19 +0530

    Add files via upload

---
+++ optimization/SVM_Optimization.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[38]:
+
+
+import numpy as np 
+import pandas as pd 
+import os
+import matplotlib.pyplot as plt
+import matplotlib
+get_ipython().run_line_magic('matplotlib', 'inline')
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import NuSVC
+from sklearn.svm import SVR
+from sklearn.utils import shuffle
+import random
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+
+
+# In[39]:
+
+
+df3 = pd.read_csv('C:/Users/Admin/Downloads/optimization/contentDataPrime.csv')
+
+
+# In[40]:
+
+
+df3.head()
+
+
+# In[41]:
+
+
+df3.isna().sum()
+
+
+# In[42]:
+
+
+df_dup3 = df3.duplicated('dataId', keep = False)
+df_dup3.value_counts()
+
+
+# In[43]:
+
+
+df3 = df3.drop_duplicates(subset ='dataId')
+
+
+# In[44]:
+
+
+df3.replace(' ?', np.nan, inplace=True)
+
+
+# In[45]:
+
+
+df3.dropna(inplace=True)
+df3.isna().sum()
+
+
+# In[16]:
+
+
+le = LabelEncoder()
+df3.rating = le.fit_transform(df3.rating)
+
+
+# In[29]:
+
+
+df3 = pd.get_dummies(df3, columns = ['contentType','title'])
+df3.head()
+
+
+# In[53]:
+
+
+categ = ['contentType','title','certificate','description','releaseYear','endYear','gross','votes','rating','length']
+
+# Encode Categorical Columns
+le = LabelEncoder()
+df3[categ] = df3[categ].apply(le.fit_transform)
+
+
+# In[54]:
+
+
+for i in range(10):
+    sample = df3.sample(frac=0.40, replace=False)
+    x = sample.iloc[:, :-1]
+    y = sample.iloc[:, -1]
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+
+    train_data = pd.concat([x_train, y_train], axis=1)
+    test_data = pd.concat([x_test, y_test], axis=1)
+
+    train_data.to_csv(f'sample_{i+1}_train.csv', index=False)
+    test_data.to_csv(f'sample_{i+1}_test.csv', index=False)
+    
+    print(f"Sample {i+1}: Train={len(train_data)}, Test={len(test_data)}")
+
+
+# In[48]:
+
+
+kernels = ['linear', 'poly', 'rbf', 'sigmoid']
+nus = [0]
+epsilons = [0]
+best_params = []
+best_scores = []
+
+results=[]
+
+
+# In[ ]:
+
+
+for i in range(10):
+    train_data = pd.read_csv(f'sample_{i+1}_train.csv')
+    test_data = pd.read_csv(f'sample_{i+1}_test.csv')
+    x_train = train_data.drop('rating', axis=1)
+    y_train = train_data['rating']
+    x_test = test_data.drop('rating', axis=1)
+    y_test = test_data['rating']
+
+    best_score = 0
+    best_param = {}
+    for k in kernels:
+        for j in range(1000):
+            nu = random.uniform(0, 1)
+            eps = random.uniform(0, 1)
+            svm = NuSVC(kernel=k, nu=nu, gamma='auto')
+            svm = SVR(kernel=k, epsilon=eps, C=1.0)
+            svm.fit(x_train, y_train)
+            score = svm.score(x_test, y_test)
+            if score > best_score:
+                best_score = score
+                best_param = {'kernel': k, 'nu': nu, 'epsilon': eps}
+    best_scores.append(best_score)
+    best_params.append(best_param)
+    results.append({'accuracy': accuracy, 'kernel': best_params['kernel'], 'nu': best_params['nu'], 'epsilon': best_params['epsilon']})
+
+
+# In[ ]:
+
+
+results_df = pd.DataFrame(results)
+results_df.index.name = 'Sample'
+display(results_df)
+
---
+++ optimization/readme.md
@@ -0,0 +1,28 @@
+# Parameter Optimization of SVM
+Assignment for UCS654
+
+## About SVM and Parameter Optimization
+
+Support Vector Machine or SVM is one of the most popular Supervised Learning algorithms, which is used for Classification as well as Regression problems. However, primarily, it is used for Classification problems in Machine Learning.
+
+Some of the most important parameters of SVM such as kernel, C, and gamma can be changed in order to achieve a higher accuracy. This is called as Hyperparameter Tuning. 
+
+We can perform this task using GridSearchCV for optimizing these parameters.
+
+In this python file, I've used a Fitness Function to optimize the parameters.
+
+## Dataset
+
+The dataset for the project has been downloaded from the Kaggle.
+[https://www.kaggle.com/datasets/kurtnakasato/imdb-100000-moviestvshows?select=contentDataPrime.csv](https://www.kaggle.com/datasets/kurtnakasato/imdb-100000-moviestvshows?select=contentDataPrime.csv)
+
+Number of Instances: 98661
+
+Number of Attributes: 11
+
+## Written By
+Name : Manvi Singla
+  
+Roll No. : 102003505
+
+Sub-Group: 3CO20
